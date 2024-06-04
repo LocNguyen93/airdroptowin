494254d73b239adf14e6732e0097c96354df0514
LocNguyen93 <loctuanjob@gmail.com>
2024-06-04 23:48:49 +0700

    Update watpoint.js

--- watpoint/watpoint.js
+++ watpoint/watpoint.js
@@ -1,39 +1,75 @@
 const axios = require("axios");
+const { getDateTimeLocal } = require("../common");
+
 const { accounts } = require("./config");
 
-async function callApi(account) {
-  let data = JSON.stringify({
-    jsonrpc: "2.0",
-    id: "miningEvent.startSession",
-    method: "miningEvent.startSession",
-    params: {
-      miningEventId: 7,
-    },
-  });
+async function callApiClaim(account) {
+  let data = '{"jsonrpc":"2.0","id":"miningEvent.startSession","method":"miningEvent.startSession","params":{"miningEventId":7}}';
 
   let config = {
-    method: "post",
+    method: 'post',
     maxBodyLength: Infinity,
-    url: "https://api.gamee.com/",
+    url: 'https://api.service.gameeapp.com/',
     headers: {
-      accept: "*/*",
-      "accept-language": "en-US,en;q=0.9,vi;q=0.8",
-      "content-type": "application/json",
-      authorization: `Bearer ${account.authorization}`,
-      "client-language": "en",
-      origin: "https://prizes.gamee.com",
-      priority: "u=1, i",
-      referer: "https://prizes.gamee.com/",
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": "Windows",
-      "sec-fetch-dest": "empty",
-      "sec-fetch-mode": "cors",
-      "sec-fetch-site": "same-site",
-      "user-agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
-      "x-install-uuid": `${account.uuid}`,
+      'accept': '*/*',
+      'accept-language': 'vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5',
+      'authorization': `Bearer ${account?.token}`,
+      'client-language': 'en',
+      'content-type': 'text/plain;charset=UTF-8',
+      'origin': 'https://prizes.gamee.com',
+      'priority': 'u=1, i',
+      'referer': 'https://prizes.gamee.com/',
+      'sec-ch-ua': '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+      'sec-ch-ua-mobile': '?1',
+      'sec-ch-ua-platform': '"Android"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'cross-site',
+      'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Mobile Safari/537.36',
+      'x-install-uuid': `${account?.uuid}`,
     },
-    data: data,
+    data: data
+  };
+
+
+  var result = await axios
+    .request(config)
+    .then((response) => {
+      return { statusCode: response?.status, ...response.data };
+    })
+    .catch(async (error) => {
+      return { statusCode: error?.response?.status };
+    });
+
+  return result;
+}
+
+async function callApiLogin(account) {
+  let data = `{"jsonrpc":"2.0","id":"user.authentication.loginUsingTelegram","method":"user.authentication.loginUsingTelegram","params":{"initData":"${account?.authorization}"}}`;
+
+  let config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: 'https://api.gamee.com/',
+    headers: {
+      'accept': '*/*',
+      'accept-language': 'vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5',
+      'client-language': 'en',
+      'content-type': 'text/plain;charset=UTF-8',
+      'origin': 'https://prizes.gamee.com',
+      'priority': 'u=1, i',
+      'referer': 'https://prizes.gamee.com/',
+      'sec-ch-ua': '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"Windows"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+      'x-install-uuid': `${account?.uuid}`,
+      'Cookie': '_nss=1'
+    },
+    data: data
   };
 
   var result = await axios
@@ -53,22 +89,25 @@ async function run() {
     let account = accounts[index];
     console.log("start account --> ", index);
     let isRun = true;
+    const result = await callApiLogin(account);
+    console.log("refresh token", result?.result?.tokens?.refresh);
+    account.token = result?.result?.tokens?.refresh;
+
     while (isRun) {
-      const response = await callApi(account);
+      const response = await callApiClaim(account);
       if (response?.statusCode === 201 || response?.statusCode === 200) {
         console.log("Claim success amount", response);
-        if (response?.success == false) isRun = false;
       } else {
         console.log("Job fail", response);
-        isRun = false;
       }
+      isRun = false;
     }
   }
 
-  console.log("Bunny --> DONE AT ", new Date());
+  console.log("DONE AT ", getDateTimeLocal());
   setTimeout(() => {
     run();
-  }, 60 * 1000 * 60);
+  }, 122 * 1000 * 60);
 }
 
 run();
