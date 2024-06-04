19f5b8e4a89708bfd6873f4de2ec511ee9b96568
Tu Van <tu.van@gosell.vn>
2024-06-04 15:19:36 +0700

    merge

--- account-config.js
+++ account-config.js
@@ -11,7 +11,7 @@ var accountConfig = {
   ACC_YESCOIN: [
     {
       index: 0,
-      token: ""
+      token: "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMzM2OTI4ODcwIiwiY2hhdElkIjoiMTMzNjkyODg3MCIsImlhdCI6MTcxNzQxMjY1NCwiZXhwIjoxNzIwMDA0NjU0LCJyb2xlQXV0aG9yaXplcyI6W10sInVzZXJJZCI6MTc5MTcwOTIxNjE1NjIwMDk2MH0.-NYtuOzX-5T7pPlyahxrIZ4sD0kYI4IauSk_6DDuy2fKim7qMO-TVMvCTT9ldurH-MnbgVup_QikmT3oIl47wg"
     },
   ],
   ACC_MDAO: [
