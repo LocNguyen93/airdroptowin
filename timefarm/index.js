a92c0d582d53e9fbfc9aa3eea13d9e602e53c823
LocNguyen93 <loctuanjob@gmail.com>
2024-06-10 12:48:42 +0700

    Update index.js

--- timefarm/index.js
+++ timefarm/index.js
@@ -42,28 +42,66 @@ async function callApiLogin(account) {
 async function callApiClaim(token) {
   let data = JSON.stringify({});
 
-  let config = {
-    method: 'post',
-    maxBodyLength: Infinity,
-    url: 'https://tg-bot-tap.laborx.io/api/v1/farming/start',
-    headers: {
-      'accept': '*/*',
-      'accept-language': 'vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5',
-      'authorization': `Bearer ${token}`,
-      'content-type': 'application/json',
-      'origin': 'https://tg-tap-miniapp.laborx.io',
-      'priority': 'u=1, i',
-      'referer': 'https://tg-tap-miniapp.laborx.io/',
-      'sec-ch-ua': '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
-      'sec-ch-ua-mobile': '?0',
-      'sec-ch-ua-platform': '"Windows"',
-      'sec-fetch-dest': 'empty',
-      'sec-fetch-mode': 'cors',
-      'sec-fetch-site': 'same-site',
-      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
-    },
-    data: data
-  };
+let config = {
+  method: 'post',
+  maxBodyLength: Infinity,
+  url: 'https://tg-bot-tap.laborx.io/api/v1/farming/finish',
+  headers: { 
+    'accept': '*/*', 
+    'accept-language': 'vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5', 
+    'authorization': `Bearer ${token}`, 
+    'content-type': 'application/json', 
+    'origin': 'https://tg-tap-miniapp.laborx.io', 
+    'priority': 'u=1, i', 
+    'referer': 'https://tg-tap-miniapp.laborx.io/', 
+    'sec-ch-ua': '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"', 
+    'sec-ch-ua-mobile': '?0', 
+    'sec-ch-ua-platform': '"Windows"', 
+    'sec-fetch-dest': 'empty', 
+    'sec-fetch-mode': 'cors', 
+    'sec-fetch-site': 'same-site', 
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
+  },
+  data : data
+};
+
+  var result = await axios
+    .request(config)
+    .then((response) => {
+      return { statusCode: response?.status, ...response.data };
+    })
+    .catch(async (error) => {
+      return { statusCode: error?.response?.status };
+    });
+
+  return result;
+}
+
+async function callApiStart(token) {
+  let data = JSON.stringify({});
+
+let config = {
+  method: 'post',
+  maxBodyLength: Infinity,
+  url: 'https://tg-bot-tap.laborx.io/api/v1/farming/start',
+  headers: { 
+    'accept': '*/*', 
+    'accept-language': 'vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5', 
+    'authorization': `Bearer ${token}`, 
+    'content-type': 'application/json', 
+    'origin': 'https://tg-tap-miniapp.laborx.io', 
+    'priority': 'u=1, i', 
+    'referer': 'https://tg-tap-miniapp.laborx.io/', 
+    'sec-ch-ua': '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"', 
+    'sec-ch-ua-mobile': '?0', 
+    'sec-ch-ua-platform': '"Windows"', 
+    'sec-fetch-dest': 'empty', 
+    'sec-fetch-mode': 'cors', 
+    'sec-fetch-site': 'same-site', 
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
+  },
+  data : data
+};
 
   var result = await axios
     .request(config)
@@ -91,6 +129,9 @@ async function run() {
     while (isRun) {
       const claimRes = await callApiClaim(token);
       console.log("Claim", claimRes);
+
+      const startRes = await callApiStart(token);
+      console.log("Start", startRes);
       isRun = false;
     }
   }
