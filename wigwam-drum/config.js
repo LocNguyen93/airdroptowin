6cabd6fd6d60a7fb30c5a5bd1c990a4e69343c04
Dung Nguyen Cong <dung.nguyen.cong@mediastep.com>
2024-06-12 18:15:47 +0700

    add wigwam-drum

---
+++ wigwam-drum/config.js
@@ -0,0 +1,22 @@
+let accounts = [
+  {
+    index: 0,
+    devAuthData: 1696078330,
+    authData:
+      "query_id=AAH6GRhlAAAAAPoZGGWfDsbM&user=%7B%22id%22%3A1696078330%2C%22first_name%22%3A%22nc%22%2C%22last_name%22%3A%22dred%22%2C%22language_code%22%3A%22vi%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1717848855&hash=2151c5740116508514b12e3b9eb87e09693d3b2355ac2816e9c285ae62c483ca",
+  },
+  {
+    index: 1,
+    devAuthData: 7201765661,
+    authData:
+      "query_id=AAEdOUItAwAAAB05Qi25oMpa&user=%7B%22id%22%3A7201765661%2C%22first_name%22%3A%22Ref%22%2C%22last_name%22%3A%22First%22%2C%22language_code%22%3A%22vi%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1717818166&hash=58865815a77c7d51fb36a57259bc7c73454f96f72c1ffa07afa60d8d7ac4826b",
+  },
+  {
+    index: 2,
+    devAuthData: 7342613446,
+    authData:
+      "query_id=AAHGY6c1AwAAAMZjpzV18hDN&user=%7B%22id%22%3A7342613446%2C%22first_name%22%3A%22Ref%22%2C%22last_name%22%3A%22Two%22%2C%22language_code%22%3A%22vi%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1717867327&hash=646b3cc465c2f4b48ea1f165ac28a8f54db276714dd42909764b77a5d819354e",
+  },
+];
+
+module.exports = { accounts };
