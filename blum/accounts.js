0f0e4acf7a72eced672c2a5e63da0b5a45142c8c
nguyenquanghuy-forwork <nguyenquanghuy.forwork@gmail.com>
2024-09-12 10:46:37 +0700

    commit

--- blum/accounts.js
+++ blum/accounts.js
@@ -1,13 +1,11 @@
 module.exports = [
   {
     name: "Lucifer One",
-    account_info:
-      "query_id=AAEimTovAwAAACKZOi-2nSAS&user=%7B%22id%22%3A7234820386%2C%22first_name%22%3A%22Lucifer%22%2C%22last_name%22%3A%22One%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1726071906&hash=54b90b19a3e81712bf53cf437381d23ab0135a73f2cbf4ff889566d2ab4acbd5",
+    account_info: "query_id=xxxxx&user=xxxxxxxxxxx",
   },
   {
     name: "Lucifer Two",
-    account_info:
-      "query_id=AAGFM-82AwAAAIUz7zbeVFP5&user=%7B%22id%22%3A7364096901%2C%22first_name%22%3A%22Lucifer%22%2C%22last_name%22%3A%22Two%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1726071832&hash=428f1187a24a75d3843e81f1192ca58f71ef76374e03930d9d3b3f24fb4ce088",
+    account_info: "query_id=xxxxx&user=xxxxxxxxxxx",
   },
   // Add more accounts if needed
 ];
--- tomarket/accounts.js
+++ tomarket/accounts.js
@@ -1,13 +1,11 @@
 module.exports = [
   {
     name: "Lucifer One",
-    account_info:
-      "query_id=AAGFM-82AwAAAIUz7zblKQPO&user=%7B%22id%22%3A7364096901%2C%22first_name%22%3A%22Lucifer%22%2C%22last_name%22%3A%22Two%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1726067503&hash=c16051f5db30b5d38808b6675fb20467f6093706255c6e90c76e975304406a47",
+    account_info: "query_id=xxxxx&user=xxxxxxxxxxx",
   },
   {
     name: "Lucifer Two",
-    account_info:
-      "query_id=AAGFM-82AwAAAIUz7zblKQPO&user=%7B%22id%22%3A7364096901%2C%22first_name%22%3A%22Lucifer%22%2C%22last_name%22%3A%22Two%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1726067503&hash=c16051f5db30b5d38808b6675fb20467f6093706255c6e90c76e975304406a47",
+    account_info: "query_id=xxxxx&user=xxxxxxxxxxx",
   },
   // Add more accounts if needed
 ];
