348ecc3c1906dda1cba4668ae0c965abd11db0a9
nguyenquanghuy-forwork <nguyenquanghuy.forwork@gmail.com>
2024-09-16 13:17:34 +0700

    commit

--- blum/blum.js
+++ blum/blum.js
@@ -5,7 +5,7 @@ const accounts = require("./accounts"); // Import accounts
 const getNodeFetch = async () => (await import("node-fetch")).default;
 
 function getRandomPoints() {
-  return Math.floor(Math.random() * (200 - 180 + 1)) + 180;
+  return Math.floor(Math.random() * (190 - 180 + 1)) + 180;
 }
 
 const TOKEN_EXPIRATION_BUFFER = 60 * 1000; // Buffer time before expiration in milliseconds
@@ -14,32 +14,28 @@ async function authenticateUser(account) {
   try {
     const { account_info, name } = account;
     const fetch = await getNodeFetch();
-    const response = await fetch(
-      "https://user-domain.blum.codes/api/v1/auth/provider/PROVIDER_TELEGRAM_MINI_APP",
-      {
-        method: "POST",
-        headers: {
-          accept: "application/json, text/plain, */*",
-          "accept-language": "en-US,en;q=0.9",
-          "content-type": "application/json",
-          lang: "en",
-          priority: "u=1, i",
-          "sec-ch-ua":
-            '"Chromium";v="128", "Not;A=Brand";v="24", "Google Chrome";v="128"',
-          "sec-ch-ua-mobile": "?0",
-          "sec-ch-ua-platform": '"Windows"',
-          "sec-fetch-dest": "empty",
-          "sec-fetch-mode": "cors",
-          "sec-fetch-site": "same-site",
-        },
-        referrerPolicy: "no-referrer",
-        body: JSON.stringify({
-          query: account_info,
-        }),
-        mode: "cors",
-        credentials: "omit",
-      }
-    );
+    const response = await fetch("https://user-domain.blum.codes/api/v1/auth/provider/PROVIDER_TELEGRAM_MINI_APP", {
+      method: "POST",
+      headers: {
+        accept: "application/json, text/plain, */*",
+        "accept-language": "en-US,en;q=0.9",
+        "content-type": "application/json",
+        lang: "en",
+        priority: "u=1, i",
+        "sec-ch-ua": '"Chromium";v="128", "Not;A=Brand";v="24", "Google Chrome";v="128"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site",
+      },
+      referrerPolicy: "no-referrer",
+      body: JSON.stringify({
+        query: account_info,
+      }),
+      mode: "cors",
+      credentials: "omit",
+    });
 
     const data = await response.json();
     console.log("Authentication Response:", data);
@@ -64,38 +60,31 @@ const readTokens = () => {
 
 // Function to check if the token is expired
 function isTokenExpired(expirationTime) {
-  return (
-    expirationTime === null ||
-    Date.now() >= expirationTime - TOKEN_EXPIRATION_BUFFER
-  );
+  return expirationTime === null || Date.now() >= expirationTime - TOKEN_EXPIRATION_BUFFER;
 }
 
 async function callFirstApi(token) {
   try {
     const fetch = await getNodeFetch();
-    const response = await fetch(
-      "https://game-domain.blum.codes/api/v1/game/play",
-      {
-        method: "POST",
-        headers: {
-          accept: "application/json, text/plain, */*",
-          "accept-language": "en-US,en;q=0.9",
-          authorization: `Bearer ${token}`, // Use the token directly
-          lang: "en",
-          "sec-ch-ua":
-            '"Chromium";v="128", "Not;A=Brand";v="24", "Google Chrome";v="128"',
-          "sec-ch-ua-mobile": "?1",
-          "sec-ch-ua-platform": '"Android"',
-          "sec-fetch-dest": "empty",
-          "sec-fetch-mode": "cors",
-          "sec-fetch-site": "same-site",
-        },
-        referrerPolicy: "no-referrer",
-        body: null,
-        mode: "cors",
-        credentials: "include",
-      }
-    );
+    const response = await fetch("https://game-domain.blum.codes/api/v1/game/play", {
+      method: "POST",
+      headers: {
+        accept: "application/json, text/plain, */*",
+        "accept-language": "en-US,en;q=0.9",
+        authorization: `Bearer ${token}`, // Use the token directly
+        lang: "en",
+        "sec-ch-ua": '"Chromium";v="128", "Not;A=Brand";v="24", "Google Chrome";v="128"',
+        "sec-ch-ua-mobile": "?1",
+        "sec-ch-ua-platform": '"Android"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site",
+      },
+      referrerPolicy: "no-referrer",
+      body: null,
+      mode: "cors",
+      credentials: "include",
+    });
 
     const data = await response.json();
     console.log("First API Response Data:", data);
@@ -109,36 +98,32 @@ async function callSecondApi(gameId, token) {
   try {
     const fetch = await getNodeFetch();
     const randomPoint = getRandomPoints();
-    const response = await fetch(
-      "https://game-domain.blum.codes/api/v1/game/claim",
-      {
-        method: "POST",
-        headers: {
-          accept: "application/json, text/plain, */*",
-          "accept-language": "en-US,en;q=0.9",
-          authorization: `Bearer ${token}`, // Use the token directly
-          "content-type": "application/json",
-          lang: "en",
-          origin: "https://telegram.blum.codes",
-          "sec-ch-ua":
-            '"Chromium";v="128", "Not;A=Brand";v="24", "Google Chrome";v="128"',
-          "sec-ch-ua-mobile": "?1",
-          "sec-ch-ua-platform": '"Android"',
-          "sec-fetch-dest": "empty",
-          "sec-fetch-mode": "cors",
-          "sec-fetch-site": "same-site",
-          "user-agent":
-            "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Mobile Safari/537.36",
-        },
-        body: JSON.stringify({
-          gameId: gameId,
-          points: randomPoint,
-        }),
-        referrerPolicy: "no-referrer",
-        mode: "cors",
-        credentials: "include",
-      }
-    );
+    const response = await fetch("https://game-domain.blum.codes/api/v1/game/claim", {
+      method: "POST",
+      headers: {
+        accept: "application/json, text/plain, */*",
+        "accept-language": "en-US,en;q=0.9",
+        authorization: `Bearer ${token}`, // Use the token directly
+        "content-type": "application/json",
+        lang: "en",
+        origin: "https://telegram.blum.codes",
+        "sec-ch-ua": '"Chromium";v="128", "Not;A=Brand";v="24", "Google Chrome";v="128"',
+        "sec-ch-ua-mobile": "?1",
+        "sec-ch-ua-platform": '"Android"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site",
+        "user-agent":
+          "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Mobile Safari/537.36",
+      },
+      body: JSON.stringify({
+        gameId: gameId,
+        points: randomPoint,
+      }),
+      referrerPolicy: "no-referrer",
+      mode: "cors",
+      credentials: "include",
+    });
 
     const textData = await response.text();
     console.log("Second API Response (Raw):", textData);
@@ -155,10 +140,7 @@ async function run() {
     const existingTokenData = tokensArray.find((a) => a.name === account.name);
 
     // Check if there is an existing token and if it is still valid
-    if (
-      existingTokenData &&
-      !isTokenExpired(existingTokenData.expirationTime)
-    ) {
+    if (existingTokenData && !isTokenExpired(existingTokenData.expirationTime)) {
       // Use existing token
       ({ name, token, expirationTime } = existingTokenData);
       console.log(`Using existing token for account: ${name}`);
@@ -181,28 +163,19 @@ async function run() {
         if (!data.gameId) {
           const { message } = data;
           if (message == "not enough play passes") {
-            throw new Error(
-              "------------ Invalid Game Chances ------------------"
-            );
+            throw new Error("------------ Invalid Game Chances ------------------");
           }
         }
         if (data.gameId) {
           const randomWaitTime = Math.floor(Math.random() * (35 - 31 + 1)) + 31;
-          console.log(
-            `Waiting ${randomWaitTime} seconds before calling API 2...`
-          );
-          await new Promise((resolve) =>
-            setTimeout(resolve, randomWaitTime * 1000)
-          );
+          console.log(`Waiting ${randomWaitTime} seconds before calling API 2...`);
+          await new Promise((resolve) => setTimeout(resolve, randomWaitTime * 1000));
           await callSecondApi(data.gameId, token); // Pass the token to the second API call
         } else {
           console.error("Game ID is undefined. Skipping API 2 call.");
         }
       } catch (error) {
-        console.error(
-          `\x1b[31mError in account ${name}:\x1b[0m`,
-          error.message
-        ); // Red color for error
+        console.error(`\x1b[31mError in account ${name}:\x1b[0m`, error.message); // Red color for error
         console.log("\x1b[33mSwitching to the next account...\x1b[0m"); // Yellow color for switching message
         console.log("\n");
         break; // Exit the loop on error and proceed to the next account
@@ -214,10 +187,7 @@ async function run() {
   }
 
   // Save all tokens to a JSON file
-  fs.writeFileSync(
-    path.join(__dirname, "tokens.json"),
-    JSON.stringify(tokensArray)
-  );
+  fs.writeFileSync(path.join(__dirname, "tokens.json"), JSON.stringify(tokensArray));
 }
 
 run();
