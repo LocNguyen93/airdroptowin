b09db5c54f7d9f4fb05c20b30323b2e10b030195
Tu Van <tu.van@stepmediasoftware.com>
2024-09-24 22:11:20 +0700

    up

--- _ecosystem.config.js
+++ _ecosystem.config.js
@@ -8,6 +8,22 @@ module.exports = {
         watch: false,
         max_memory_restart: "50M",
       },
+      {
+        name: "Seed",
+        script: "./seed/index.js",
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: "50M",
+      },
+      {
+        name: "Time",
+        script: "./time/index.js",
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: "50M",
+      },
     ],
   };
   
